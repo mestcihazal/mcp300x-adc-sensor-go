b314fb9951c0440f22288163502b62a0acedf041
Hazal Mestci <101270785+mestcihazal@users.noreply.github.com>
2023-12-13 11:29:47 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+// package main is a module that implements a sensor model of mcp300x
+package main
+
+import (
+	"context"
+
+	"github.com/edaniels/golog"
+  "github.com/mestcihazal/mcp300x-adc-sensor-go/mcp300x"
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/module"
+	"go.viam.com/utils"
+)
+
+func main() {
+	utils.ContextualMain(mainWithArgs, golog.NewDevelopmentLogger("mcp300x-adc-sensor-go"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error {
+	sensorModule, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	sensorModule.AddModelFromRegistry(ctx, sensor.API, mcp300x.Model)
+
+	err = sensorModule.Start(ctx)
+	defer sensorModule.Close(ctx)
+	if err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+	return nil
+}
