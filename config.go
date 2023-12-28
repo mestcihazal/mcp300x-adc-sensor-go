eed00f02151e19ca081840cca39d871913f1174e
Hazal Mestci <101270785+mestcihazal@users.noreply.github.com>
2023-12-28 12:28:07 -0500

    Create config.go

---
+++ config.go
@@ -0,0 +1,21 @@
+package mcp300x
+
+import (
+	"errors"
+)
+
+type Mcp300xConfig struct {
+	Pins       map[string]int `json:"pins"`
+	SpiBus     string         `json:"spi_bus"`
+	ChipSelect string         `json:"chip_select"`
+}
+
+func (config *Mcp300xConfig) Validate(path string) ([]string, error) {
+	if config.SpiBus == "" {
+		return nil, errors.New("you need to specify the SPI bus")
+	}
+	if config.ChipSelect == "" {
+		return nil, errors.New("you need to specify the chip select pin")
+	}
+	return []string{}, nil
+}
